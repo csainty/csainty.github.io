d93e98bc745e22e786cb923da089779b44dd8600
Chris Sainty <csainty@hotmail.com>
2015-07-25 11:14:02 +0200

    Replace Rakefile with shell script

---
+++ publish.sh
@@ -0,0 +1,11 @@
+rm -rf tmpdir
+mkdir tmpdir
+rsync -av --exclude='*.sh' --exclude='Vagrantfile' _site/ tmpdir/
+cd tmpdir
+git init
+git add .
+git commit -m "Site Updated"
+git remote add origin git@github.com:csainty/csainty.github.io.git
+git push origin master --force
+cd ..
+rm -rf tmpdir
