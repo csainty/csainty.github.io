8e113432c035b8c92b6ad6bb2f557835c97776da
Chris Sainty <csainty@hotmail.com>
2015-08-02 19:47:29 +0200

    Made publish script callable from outside the vm

--- publish.sh
+++ publish.sh
@@ -1,12 +1,13 @@
-jekyll build
-rm -rf tmpdir
-mkdir tmpdir
-rsync -av --exclude='*.sh' --exclude='Vagrantfile' _site/ tmpdir/
-cd tmpdir
-git init
-git add .
-git commit -m "Site Updated"
-git remote add origin git@github.com:csainty/csainty.github.io.git
-git push origin master --force
-cd ..
-rm -rf tmpdir
+vagrant ssh -c "cd /vagrant &&
+jekyll build &&
+rm -rf tmpdir &&
+mkdir tmpdir &&
+rsync -av --exclude='*.sh' --exclude='Vagrantfile' _site/ tmpdir/ &&
+cd tmpdir &&
+git init &&
+git add . &&
+git commit -m \"Site Updated\" &&
+git remote add origin git@github.com:csainty/csainty.github.io.git &&
+git push origin master --force &&
+cd .. &&
+rm -rf tmpdir"
